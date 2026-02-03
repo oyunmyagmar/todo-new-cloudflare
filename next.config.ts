a9134ac223394d77087338d273f9459d0f7d2f57
oyunmyagmar <25LP4018@nest.edu.mn>
2026-02-04 08:42:38 +0900

    for deployment

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   eslint: {
-    // Build хийх үед ESLint алдаа гарсан ч үргэлжлүүлнэ
     ignoreDuringBuilds: true,
   },
   typescript: {
-    // TypeScript-ийн алдааг мөн адил үл тоосно
     ignoreBuildErrors: true,
   },
 };
